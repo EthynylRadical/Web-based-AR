ba09ec7858c7542f64a3669f5f773aed78647da0
radical ethynyl <ethynyl@outlook.com>
2021-03-07 14:16:07 +0800

    3.7

--- demo1/server.js
+++ demo1/server.js
@@ -3,34 +3,25 @@ var fs = require('fs');//引入文件读取模块
 
 var htmlRoot = 'D:\\Github\\Web-based-AR\\demo1\\main.html'
 
-var server= http.createServer(function(req,res){
+var server= http.createServer(function(req,res)
+{
 
-    var url = req.url; 
-    //客户端输入的url，例如如果输入localhost:8888/index.html
-    //那么这里的url == /index.html 
-
+    var url = req.url;
     var file = htmlRoot;
     console.log(url);
 
-    fs.readFile( file , function(err,data){
-    /*
-        err为文件路径
-        data为回调函数
-            回调函数的一参为读取错误返回的信息，返回空就没有错误
-            data为读取成功返回的文本内容
-    */
+    fs.readFile( file , function(err,data)
+    {
         if(err){
             res.writeHeader(404,{
                 'content-type' : 'text/html;charset="utf-8"'
             });
-            res.write('<h1>404错误</h1><p>你要找的页面不存在</p>');
-            res.end();
+            res.end('<h1>404错误</h1><p>你要找的页面不存在</p>');
         }else{
             res.writeHeader(200,{
                 'content-type' : 'text/html;charset="utf-8"'
             });
-            res.write(data);//将index.html显示在客户端
-            res.end();
+            res.end(data);
         }
 
     });
