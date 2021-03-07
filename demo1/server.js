5bf5150503efbd6485838b22c463d440eca13816
radical ethynyl <ethynyl@outlook.com>
2021-03-07 15:09:02 +0800

    del node

--- demo1/server.js
+++ demo1/server.js
@@ -1,30 +1,57 @@
 var http = require('http');
 var fs = require('fs');//引入文件读取模块
 
-var htmlRoot = 'D:\\Github\\Web-based-AR\\demo1\\main.html'
+var fileRoot = 'D:\\Github\\Web-based-AR\\demo1\\main.html'
+
+function parseJSON(req,res,next){
+    var arr = [];
+    req.on("data",function(data){
+        arr.push(data);
+    });
+    req.on("end",function(){
+        var data= Buffer.concat(arr).toString(),ret;
+        try{
+            var ret = JSON.parse(data);
+        }catch(err){}
+        req.body = ret;
+        next();
+    })
+}
 
 var server= http.createServer(function(req,res)
 {
 
     var url = req.url;
-    var file = htmlRoot;
-    console.log(url);
+    if (req.url === '/' && req.method.toLowerCase() === 'get') {
+        var file = fileRoot + 'camera.html';
+        console.log(url);
 
-    fs.readFile( file , function(err,data)
-    {
-        if(err){
-            res.writeHeader(404,{
-                'content-type' : 'text/html;charset="utf-8"'
-            });
-            res.end('<h1>404错误</h1><p>你要找的页面不存在</p>');
-        }else{
-            res.writeHeader(200,{
-                'content-type' : 'text/html;charset="utf-8"'
-            });
-            res.end(data);
-        }
+        fs.readFile( file , function(err,data)
+        {
+            if(err){
+                res.writeHeader(404,{
+                    'content-type' : 'text/html;charset="utf-8"'
+                });
+                res.end('<h1>404错误</h1><p>你要找的页面不存在</p>');
+            }else{
+                res.writeHeader(200,{
+                    'content-type' : 'text/html;charset="utf-8"'
+                });
+                res.end(data);
+            }
 
-    });
+        });
+    }
+    else if (req.url === '/' && req.method.toLowerCase() === 'post') {
+        var pData = '';
+        req.on('data', function (chunk) {
+            pData += chunk;
+        });
+        req.on('end', function () {
+
+        })
+    }
+
 
 }).listen(8080);
 
--- demo1/temp.js
+++ demo1/temp.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 
 
-app.use("/", express.static('public'));
+app.use(express.static("‪C:\\Users\\Ethynyl Radical\\Desktop\\PycharmProject"));
 
 //Setting the router
 app.get("/", (req, res) => {
-    res.send()
+    res.send('Hello world')
+});
+var server = app.listen(8080, function () {
+    var host = server.address().host;
+    var port = server.address().port;
+
+    console.log(`Running on http://${host}:${port}`)
 })
